c4be97e74f6b61db77830f3afbb0d47e8235c541
martinejaw <martinezequieljaworski@gmail.com>
2021-01-21 11:41:56 -0300

    Anda

--- __tests__/test_get.js
+++ __tests__/test_get.js
@@ -4,6 +4,6 @@ const app = require("../app");
 describe("Test the root path", () => {
   test("It should response the GET method", async () => {
     const response = await request(app).get("/");
-    expect(response.statusCode).toBe(201);
+    expect(response.statusCode).toBe(200);
   });
 });
