0420cdf92d1e8aaf70a3a2a89ec1abc9cfad6109
martinejaw <martinezequieljaworski@gmail.com>
2021-01-21 11:39:29 -0300

    Probando

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send({ hola: 'Hello World!' })
+})
+
+app.get('/recetas', (req, res) => {
+    res.send('Ejemplo')
+})
+
+module.exports = app
